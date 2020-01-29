565845c11aaa11ebedff1e724ecff0ea02ff0879
jonathan343 <jonathangee09@gmail.com>
2020-01-28 21:24:56 -0600

    Initial Commit

---
+++ TAMU_UNICEF_APP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TAMU_UNICEF_APP
+//
+//  Created by Jonathan Gaytan on 1/28/20.
+//  Copyright © 2020 TAMU_UNICEF. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
